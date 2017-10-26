c987eddec8055a0777cba881a03bece04fd24180
Subir <32188427+subir-unified@users.noreply.github.com>
2017-10-26 16:10:26 +0530

    login

--- html/js/init.js
+++ html/js/init.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	AOS.init();
 });
-
+$(window).on('load', function(){
+	$('body').addClass('loaded');
+	setTimeout(function(){
+		$('.log-content').addClass('visible-elem');
+	},1300);
+});
 
