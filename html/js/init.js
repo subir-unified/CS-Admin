8af57e407bc39ae7b664f5366ae0df6e4dc6c1f5
Subir <32188427+subir-unified@users.noreply.github.com>
2017-10-27 12:07:47 +0530

    formating code

--- html/js/init.js
+++ html/js/init.js
@@ -1,12 +1,54 @@
-$(document).ready(function(){
-	AOS.init();
-});
 $(window).on('load', function(){
 	$('body').addClass('loaded');
 	setTimeout(function(){
 		$('.log-content').addClass('visible-elem');
 	},1300);
 });
+
+$(document).ready(function(){
+	AOS.init();
+
+	$('.refine-link').on('click', function(){
+		$(this).parents('.refine-wrap').toggleClass('active');
+			var nextContent = $(this).next();
+			$(nextContent).slideToggle(600, function() {
+			setTimeout(
+			pscroll('refine-list-wrap-inr'),1000)
+		});
+	});
+
+	$('.refine-wrap').on('click', function(event){
+		event.stopPropagation();
+	});
+
+	$(document).on('click', function(){
+	  $('.refine-list-wrap').slideUp(600);
+	  $('.refine-wrap').removeClass('active');
+	});
+
+	$('.topSearch-group .search-btn').on('click', function(){
+		$('.topSearch-group').toggleClass('active');
+	});
+
+	$('.sidebar-tgl-bar').on('click', function(){
+		$('.sidebar').toggleClass('off');
+		$('.main-wrapper').toggleClass('full');
+	});
+
+	$('.ntfDropdown').on('show.bs.dropdown', function () {
+	  setTimeout(function(){pscroll('dropdown-menu-list-wrap');}, 200);
+	});
+
+	  $('.msgDropdown').on('show.bs.dropdown', function () {
+	  setTimeout(function(){pscroll('msgDropdown-pScroll')},200);
+	});
+
+	  $('.caldDropdown').on('show.bs.dropdown', function () {
+	  setTimeout(function(){pscroll('caldDropdown-pScroll')},200);
+	});
+
+});
+
 
 (function() {
 	// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
@@ -41,3 +83,31 @@ $(window).on('load', function(){
 		}
 	}
 })();
+
+
+function pscroll(x){
+	  const ps = new PerfectScrollbar('.'+x, {
+	  wheelSpeed: 2,
+	  wheelPropagation: true,
+	  minScrollbarLength: 20
+	});
+}
+
+
+function heightCal(){
+	var wHeight = $(window).height();
+	var calHeight = wHeight - 100;
+	$('.refine-list-wrap').css('height', calHeight);
+}
+$(document).ready(function(){ 
+	heightCal();
+});
+$(window).resize(function(){
+	heightCal();
+});
+
+
+
+
+
+
